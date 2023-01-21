b0cece20192a94aae15e1d2cdde54d8372e2e326
Vishnu <codemaker2015@gmail.com>
2023-01-21 09:48:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Editor from "./Editor";
+import './styles.css'
+export default () => {
+
+
+  return (
+    <div >
+      Quill Editor
+      <Editor />
+      
+
+    </div>
+  );
+};
